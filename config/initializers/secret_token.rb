4b844272fc72c832ccf792d00aee784e84e548d5
Wade Xing <iamxingxing@gmail.com>
2012-07-14 18:08:41 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kabam::Application.config.secret_token = 'b2b14c8d16825ea9bb87c9aae429dddadecb1241f111a7a4cb329cc3cfba42e727850f362c8374fe41cc8a6326653d486e72ab879d594be78d24a4c4fa4cf298'
